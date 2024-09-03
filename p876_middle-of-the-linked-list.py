f41b27d160514b16752d02d2e9b24a02ad194b1d
Sumedh Junghare <jsumedh10@gmail.com>
2024-09-03 19:14:54 +0530

    Create p876_middle-of-the-linked-list.py

---
+++ p876_middle-of-the-linked-list.py
@@ -0,0 +1,17 @@
+# 876. Middle of the Linked List
+# runtime - 31ms
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        slow_ptr = head
+        fast_ptr = head
+
+        while(fast_ptr and fast_ptr.next):
+            slow_ptr = slow_ptr.next
+            fast_ptr = fast_ptr.next.next
+
+        return slow_ptr
