2f0e0a3b59ab6455f75124af006015bd8c4fb08a
Sumedh Junghare <jsumedh10@gmail.com>
2024-09-15 12:06:46 +0530

    Update p3285_Find_Indices_of_Stable_Mountains.py

--- p3285_Find_Indices_of_Stable_Mountains.py
+++ p3285_Find_Indices_of_Stable_Mountains.py
@@ -1,5 +1,13 @@
 # 3285. Find Indices of Stable Mountains
 # Run time - 41ms
+'''
+Example-
+Input: height = [1,2,3,4,5], threshold = 2
+Output: [3,4]
+
+Input: height = [10,1,10,1,10], threshold = 3
+Output: [1,3]
+'''
 class Solution:
     def stableMountains(self, height: List[int], threshold: int) -> List[int]:
         ans_list = []
