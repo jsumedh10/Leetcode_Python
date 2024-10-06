a53cf76071a5b5f39d49d5868a7e2d631fec2800
Sumedh Junghare <jsumedh10@gmail.com>
2024-10-06 14:02:24 +0530

    Update p27_Remove_Element.py

--- p27_Remove_Element.py
+++ p27_Remove_Element.py
@@ -1,19 +1,34 @@
 # 27. Remove Element
-# Only 101/115 TCs passed with this code
+# Run Time - 33ms
 
 class Solution:
     def removeElement(self, nums: List[int], val: int) -> int:
-        first = 0
-        last = len(nums)-1
-        while(first <= last):
-            if nums[last] == val:
-                last -= 1
-            if nums[first] == val:
-                nums[first], nums[last] = nums[last], nums[first]
-                last -= 1
-            first += 1
+        ptr = 0
 
-            #print(" ".join([str(x) for x in nums]))
+        for i in range(len(nums)):
+            if nums[i] != val:
+                nums[ptr] = nums[i]
+                ptr += 1
+
+        return ptr
+
+
+# Only 101/115 TCs passed with this code
+
+# class Solution:
+#     def removeElement(self, nums: List[int], val: int) -> int:
+#         first = 0
+#         last = len(nums)-1
+#         while(first <= last):
+#             if nums[last] == val:
+#                 last -= 1
+#             if nums[first] == val:
+#                 nums[first], nums[last] = nums[last], nums[first]
+#                 last -= 1
+#             first += 1
+
+#             #print(" ".join([str(x) for x in nums]))
             
 
-        return first
+#         return first
+
